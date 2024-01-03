d1d7d116187682d3f1278099c1d20143a42fa062
Enzo <Enzo>
2024-01-02 23:45:34 -0300

    creating a button test with tamagui

--- app/home.tsx
+++ app/home.tsx
@@ -1,18 +1,26 @@
 import React from "react";
 import { View, Text } from "react-native";
 import { Button } from 'tamagui'
-import { Airplay } from '@tamagui/lucide-icons'
+import { Home } from '@tamagui/lucide-icons'
 
 const HomePage = () => {
     const onClick = () => {
-        console.log('Clicked')
+        console.log('Button home page has been clicked')
     }
     return (
         <View style={{flex:1, alignItems: 'center', justifyContent: 'center', gap: 10}}>
             <Text>This is a real home page.</Text>
             <Button
                 alignSelf="center" 
-                icon={Airplay} 
+                variant="outlined"
+                pressTheme={true}
+                shadowColor='black'
+                pressStyle={{
+                    backgroundColor: 'black'
+                }}
+                color='white'
+                backgroundColor='purple'
+                icon={Home} 
                 size="$6"
                 onPress={onClick}>
                 Funcionando
